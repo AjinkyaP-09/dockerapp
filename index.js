3ef53f27eb9640105d8a7c215c5a42b5aea03482
Ajinkya <pameajinkya09@gmail.com>
2025-04-26 04:55:19 +0000

    for build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-//d sfRespond with "Hello World" when a GET request is made to the root "/"
+//Respond with "Hello World" when a GET request is made to the root "/"
 
 app.get('/', (req, res) => {
 
